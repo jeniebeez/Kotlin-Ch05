e537282d5d580d5a0096b0f612034dd57afd9c0e
jeniebeez <richardloyj@gmail.com>
2021-06-21 17:06:18 +0800

    c5-8

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,11 +1,10 @@
 fun main(args:Array<String>){
-    val greetingFunction ={
-        playerName:String, numBuildings:Int ->
+    runSimulation("Guyal"){
+        playerName, numBuildings ->
         val currentYear = 2021
         println("增加$numBuildings 個房子")
         "歡迎來到Sim村莊，$playerName！(copyright $currentYear)"
     }
-    runSimulation("Guyal", greetingFunction)
 }
 
 fun runSimulation(playerName:String,
