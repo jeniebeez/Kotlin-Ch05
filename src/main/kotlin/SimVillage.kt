9421a2b2d50d4171536824b63f82ac9a0f4abd70
jeniebeez <richardloyj@gmail.com>
2021-06-21 16:57:23 +0800

    c5-7

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -5,5 +5,12 @@ fun main(args:Array<String>){
         println("增加$numBuildings 個房子")
         "歡迎來到Sim村莊，$playerName！(copyright $currentYear)"
     }
-    println(greetingFunction("Guyal",2))
+    runSimulation("Guyal", greetingFunction)
+}
+
+fun runSimulation(playerName:String,
+                  greetingFunction:(String,Int)-> String){
+    //Randomly select 1,2 or 3
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
