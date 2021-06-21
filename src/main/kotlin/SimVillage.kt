916dfeeecbe17035a3e6ae675d496a2e8b1a6a75
jeniebeez <richardloyj@gmail.com>
2021-06-21 17:27:32 +0800

    c5-13

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,23 +1,19 @@
 fun main(args:Array<String>){
-    runSimulation("Guyal",
-        ::printConstructionCost){
-        playerName, numBuildings ->
-        val currentYear = 2021
-        println("增加$numBuildings 個房子")
-        "歡迎來到Sim村莊，$playerName！(copyright $currentYear)"
+    runSimulation()
+}
+
+fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction():(String) -> String{
+    val structureType = "醫院"
+    var numBuildings = 5
+    return {playerName:String ->
+        val currentYear =2021
+        numBuildings +=1
+        println("增加 $numBuildings 個$structureType")
+        "歡迎來到Sim村莊，$playerName!(copyright $currentYear)"
     }
-}
-
-inline fun runSimulation(playerName:String,
-                         costPrinter:(Int) -> Unit,
-                  greetingFunction:(String,Int)-> String){
-    //Randomly select 1,2 or 3
-    val numBuildings = (1..3).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-
-fun printConstructionCost(numBuildings:Int){
-    val cost = 500
-    println("建築成本: ${cost*numBuildings}")
 }
