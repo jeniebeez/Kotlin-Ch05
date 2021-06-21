2acd444eee48b3aa5b0e36ba717d1b8d8a13e037
jeniebeez <richardloyj@gmail.com>
2021-06-21 16:45:33 +0800

    c5-2

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,6 +1,7 @@
 fun main(args:Array<String>){
-    println({
+    val greetingFunction:() -> String ={
         val currentYear = 2021
         "歡迎來到Sim村莊，鎮長！(copyright $currentYear)"
-    }())
+    }
+    println(greetingFunction())
 }
